ab34e4f9a645eb48608b63e509a55b01852d559a
zakariamhirsii-hub <zakaria.mhirsii@gmail.com>
2026-05-13 11:26:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import discord
 import os
 from discord.ext import commands
-
-# Load environment variables
 from dotenv import load_dotenv
-load_dotenv()
+
+load_dotenv()  # Not strictly needed on Render if you set env vars directly
 
 intents = discord.Intents.all()
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
-    await bot.change_presence(activity=discord.Streaming(name='streaming-name', url='https://www.twitch.tv/urtwitchusername'))
+    print(f'Logged in as {bot.user}')
+    await bot.change_presence(activity=discord.Streaming(
+        name='streaming-name', 
+        url='https://www.twitch.tv/urtwitchusername'
+    ))
 
-# Get token from environment variable
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-
 if not TOKEN:
-    raise ValueError("No token found. Please set DISCORD_BOT_TOKEN in your .env file or environment variables")
+    raise ValueError("DISCORD_BOT_TOKEN environment variable not set")
 
 bot.run(TOKEN)
